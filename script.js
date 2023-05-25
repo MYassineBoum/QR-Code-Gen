2d1105957a3197c63b3fb7ebb6eaefb048a51fb9
Mohammed-Yassine BOUMEHDI <myboumehdi.2002@gmail.com>
2023-05-25 19:43:46 +0000

    improve

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let button=document.getElementById("btn");
 let input=document.getElementById("lien");
 let qrCode=document.getElementById("qrCode");
+let refresh=document.getElementById("refresh");
+
+refresh.hidden=true;
 qrCode.hidden=true;
 
 button.addEventListener("click",function(){
@@ -11,7 +14,12 @@ button.addEventListener("click",function(){
         qrCode.src=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`;
         qrCode.addEventListener("load",function(){
             qrCode.hidden=false;
+            refresh.hidden=false;
             button.innerText="Generate";
         });
     }
+});
+
+refresh.addEventListener("click",function(){
+    location.reload();
 });
